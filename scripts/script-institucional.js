8f90f9e6c6c09c6edbe01fd03195b4d91b77664d
vitormoreiran <129230152+vitormoreiran@users.noreply.github.com>
2024-12-10 02:03:52 -0300

    att

--- scripts/script-institucional.js
+++ scripts/script-institucional.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Seleciona todas as imagens da galeria
-    const images = document.querySelectorAll(".image-section img");
+document.addEventListener("DOMContentLoaded", () => { 
+    // Seleciona todas as imagens das galerias, incluindo as da .image-section-three
+    const images = document.querySelectorAll(".image-section img, .image-section-three img");
     const modal = document.getElementById("modal");
     const modalImg = document.getElementById("modal-img");
-  
+
     // Função para abrir o modal
     images.forEach((img) => {
-      img.addEventListener("click", () => {
-        modal.classList.remove("hidden");
-        modalImg.src = img.src;
-      });
+        img.addEventListener("click", () => {
+            modal.classList.remove("hidden");
+            modalImg.src = img.src;
+        });
     });
-  
+
     // Função para fechar o modal ao clicar fora da imagem
     modal.addEventListener("click", (e) => {
-      if (e.target !== modalImg) {
-        modal.classList.add("hidden");
-        modalImg.src = ""; // Remove a imagem do modal para evitar carregamento desnecessário
-      }
+        if (e.target !== modalImg) {
+            modal.classList.add("hidden");
+            modalImg.src = ""; // Remove a imagem do modal para evitar carregamento desnecessário
+        }
     });
-  });
-  
+});
